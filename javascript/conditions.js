89198983c9bafbe6424bdc980c7c97087d476d4c
pallavi4e8 <kasireddypallavi1974@gmail.com>
2026-03-12 12:20:58 +0530

    Add files via upload

---
+++ javascript/conditions.js
@@ -0,0 +1,33 @@
+//  let x=11;
+//  if(x<10){
+//      console.log("x is lessthan 10");
+//  }
+//      else{
+//        console.log("x is greathan 10");
+//     }
+
+
+//  let age=16;
+//  let d2=false
+//  if(age>18||d2==true){
+//      console.log("we can drive");
+//  }
+//      else{
+//        console.log("we cannot drive");
+//     }
+
+
+let adhar=false
+let pan=false
+if(adhar){
+    console.log("you can open the account in the bank-using adhar")
+}
+    else if(pan){
+        console.log("you can open the account in the bank-using pan")
+    }
+ else{
+    console.log("you cannot open the account in the bank")
+    }
+
+
+
