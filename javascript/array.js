d8451ae51250bf7a85baa7ffd379a847b78a2a46
pallavi4e8 <kasireddypallavi1974@gmail.com>
2026-03-13 10:57:14 +0530

    Add files via upload

---
+++ javascript/array.js
@@ -0,0 +1,21 @@
+// let score=[10,20,30,40,50];
+// console.log(score[2]);
+// console.log(score[3]);
+
+// let arr=[1,2,3];
+// arr.push(4);
+// console.log(arr);
+
+// let arr=[1,2,3];
+// arr.shift(2);
+// console.log(arr);
+
+
+// let arr=[1,2,3];
+// arr.unshift(2);
+// console.log(arr);
+
+
+
+// let arr=[1,2,3];
+// console.log(arr.length);
